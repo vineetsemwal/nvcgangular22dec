862e3ca65abfece42c2e8eff936fb7ba96ddb7ae
vineetsemwal <vineetsemwal82@gmail.com>
2023-01-05 21:42:09 +0530

    string assignment and basic demo added

---
+++ StringUtil.java
@@ -0,0 +1,34 @@
+
+class StringUtil{
+
+public static void main(String args){
+StringUtil util=new StringUtil();
+String result=util.concat("hello", "world");
+System.out.println(result);
+
+
+}
+
+/*
+concat two string and return result
+*/
+String concat(String arg1, String arg2){
+String result=arg1+arg2;
+return result;
+
+}
+
+
+/*
+Find all the vowels in string and convert into ‘b’ in all the vowels and should return on StringBuilder.
+
+StringBuilder convertTo(String input){
+
+
+}
+*/
+
+
+}
+
+
